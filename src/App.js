c23894440163645f61b9b9e294b44fc051c3b2f2
Agustin Pereira <aggggus36@gmail.com>
2022-12-21 22:22:42 -0300

    primer entrega proyecto react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import BasicExample from './navbar';
+
+function App() {
+  return (
+    <div>
+      <BasicExample/>
+    </div>
+  );
+}
+
+export default App;
+
+
